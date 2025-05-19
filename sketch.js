7db1bbe413c222fa03e2d6361cdb552e2cc3d12e
Ren <tay.renfred@gmail.com>
2025-05-20 10:15:55 +1200

    20/05/25 1015
    Need to comment every lines' use.

--- sketch.js
+++ sketch.js
@@ -1,71 +1,67 @@
-let sourceImg = null;
-let maskImg = null;
-let renderCounter = 0;
+let sourceImg=null;
+let maskImg=null;
+let renderCounter=0;
 
 // change these three lines as appropiate
 let sourceFile = "input_1.jpg";
-let maskFile = "mask_1.png";
+let maskFile   = "mask_1.png";
 let outputFile = "output_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
   maskImg = loadImage(maskFile);
-  // console.log(p5.Renderer2D);
-
 }
-function setup() {
+
+function setup () {
   let main_canvas = createCanvas(1920, 1080);
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
   noStroke();
-// DEFAULT
-  // background(255, 0, 0);
-// EXAMPLE
   background(0, 0, 128);
   sourceImg.loadPixels();
   maskImg.loadPixels();
+  colorMode(HSB);
 }
 
-function draw() {
-  // pixel overlay
-  // for(let i=0;i<4000;i++) { // drawing  a limit of 3999 "dots"
-  //   let x = floor(random(sourceImg.width));
-  //   let y = floor(random(sourceImg.height));
-  //   let pixData = sourceImg.get(x, y);
-  //   let maskData = maskImg.get(x, y);
-  //   fill(pixData);
-  //   if(maskData[0] > 128) {
-  //     let pointSize = 10;
-  //     ellipse(x, y, pointSize, pointSize);
-  //   }
-  //   else {
-  //     let pointSize = 20;
-  //     rect(x, y, pointSize, pointSize);
-  //   }
-  // }
-
-  // Gray background
-  let j = renderCounter;
+function draw () {
+  let num_lines_to_draw = 40;
   // get one scanline
-  for (let i = 0; i < 1920; i++) {
-    let pix = sourceImg.get(i, j);
-    let mask = maskImg.get(i, j);
-    if (mask[0] > 128) {
-      // draw the full pixels
-      set(i, j, pix);
-    }
-    else {
+  for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<1080; j++) {
+    for(let i=0; i<1920; i++) {
+      colorMode(RGB);
+      let pix = sourceImg.get(i, j);
+      // create a color from the values (always RGB)
+      let col = color(pix);
+      let mask = maskImg.get(i, j);
+
+      colorMode(HSB, 360, 100, 100);
       // draw a "dimmed" version in gray
-      let gray_color = 54 + pix[1] / -100;// limit: (-25 - 253) + pix[1] / 
-      set(i, j, gray_color);
+      let h = hue(col);
+      let s = saturation(col);
+      let b = brightness(col);
+
+      if(mask[0] > 128) {
+        // draw the full pixels
+        // let new_sat = map(s, 0, 100, 50, 100);
+        let new_brt = map(b, 0, 100, 50, 100);
+        // let new_hue = map(h, 0, 360, 180, 540);
+        let new_col = color(0, s, new_brt);
+        set(i, j, new_col);
+      }
+      else {
+        // let new_brt = map(b, 0, 100, 20, 40);
+        let new_brt = map(b, 0, 100, 100, 0);
+        let new_col = color(h, 0, new_brt);
+        // let new_col = color(h, s, b);
+        set(i, j, new_col);
+      }
     }
   }
+  renderCounter = renderCounter + num_lines_to_draw;
   updatePixels();
-  
-  renderCounter = renderCounter + 1;
-  if (renderCounter > 1080) {
-    // if(renderCounter > 10) {
+  // print(renderCounter);
+  if(renderCounter > 1080) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
